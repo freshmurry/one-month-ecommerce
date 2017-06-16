d89cdd03dc0cb44de96267639e4ab1e286b7d4dc
FreshMurry <lawrencemurry@yahoo.com>
2017-06-16 01:51:13 +0000

    Added new product images

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ jjhairston = Product.create(
     details: "You\'ll receive (1) Song Chordz", 
     description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
 
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
+            <p>In this series, you will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
             <p><strong>What You\'ll Receive</strong></p>
             <ul class="no-indent">
                 <li>Sheet music written specifically for ear-trained learners</li>
@@ -71,61 +71,7 @@ worth_saving_product = Product.create(
     details: "You\'ll receive (1) Song Chordz", 
     description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
 
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
-            <p><strong>What You\'ll Receive</strong></p>
-            <ul class="no-indent">
-                <li>Sheet music written specifically for ear-trained learners</li>
-                <li>Basic chord progressions</li>
-                <li>Contemporary playing style</li>
-                <li>Notation of Intro, Verse, Bridge Ending, etc.</li>
-            </ul>}, 
-    author_description: %{                
-    <p>Hey, I'm Lawrence Murry, Founder of Song Chordz. I created Song Chordz as a way to have a fast and easy access to tons of popular, contemporary, 
-	and traditional song chords for beginning to intermediate ear-trained keyboard players.</p>
-	<p>Why purchase our chordz? I have a deep passion for music and playing the keyboad. A ear-traind keyboard player myself, I've been honing craft 
-	for 16 years. I've played mostly in church and school events while in college. My goal is to help you and other musicians expand and grow your talent.</p>
-
-	<p>Follow me on Twitter <a href="https://twitter.com/freshmurry" target="_blank">@freshmurry</a></p>})
-	
-salvation_product = Product.create(
-  	title: "Hallelujah Salvation and Glory", 
-    subtitle: "By N/A ", 
-    chordz: "50",
-    author_image_name: "murry.png",
-    price: "4.99",
-    sku: "CHORDZ2", 
-    download_url: "https://s3.amazonaws.com/one-month-stripe-payments/LawrenceMurryResume.pdf", 
-    details: "You\'ll receive (1) Song Chordz", 
-    description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
-
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
-            <p><strong>What You\'ll Receive</strong></p>
-            <ul class="no-indent">
-                <li>Sheet music written specifically for ear-trained learners</li>
-                <li>Basic chord progressions</li>
-                <li>Contemporary playing style</li>
-                <li>Notation of Intro, Verse, Bridge Ending, etc.</li>
-            </ul>}, 
-    author_description: %{                
-    <p>Hey, I'm Lawrence Murry, Founder of Song Chordz. I created Song Chordz as a way to have a fast and easy access to tons of popular, contemporary, 
-	and traditional song chords for beginning to intermediate ear-trained keyboard players.</p>
-	<p>Why purchase our chordz? I have a deep passion for music and playing the keyboad. A ear-traind keyboard player myself, I've been honing craft 
-	for 16 years. I've played mostly in church and school events while in college. My goal is to help you and other musicians expand and grow your talent.</p>
-
-	<p>Follow me on Twitter <a href="https://twitter.com/freshmurry" target="_blank">@freshmurry</a></p>})
-	
-Oh_give_thanks_product = Product.create(
-	title: "Oh Give Thanks Unto The Lord", 
-    subtitle: "By Judith Christie Mcallister", 
-    chordz: "50",
-    author_image_name: "murry.png", 
-    price: "4.99",
-    sku: "CHORDZ2", 
-    download_url: "https://s3.amazonaws.com/one-month-stripe-payments/LawrenceMurryResume.pdf", 
-    details: "You\'ll receive (1) Song Chordz in the Key of C#", 
-    description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
-
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
+            <p>In this series, you will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
             <p><strong>What You\'ll Receive</strong></p>
             <ul class="no-indent">
                 <li>Sheet music written specifically for ear-trained learners</li>
@@ -152,7 +98,7 @@ the_sun_product = Product.create(
     details: "You\'ll receive (1) Song Chordz", 
     description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
 
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
+            <p>In this series, you will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
             <p><strong>What You\'ll Receive</strong></p>
             <ul class="no-indent">
                 <li>Sheet music written specifically for ear-trained learners</li>
@@ -179,7 +125,7 @@ more_than_anthything_product = Product.create(
     details: "You\'ll receive (1) Song Chordz", 
     description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
 
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
+            <p>In this series, you will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
             <p><strong>What You\'ll Receive</strong></p>
             <ul class="no-indent">
                 <li>Sheet music written specifically for ear-trained learners</li>
@@ -206,7 +152,7 @@ dont_quit_product = Product.create(
     details: "You\'ll receive (1) Song Chordz", 
     description: %{<p>If you are a beginner to intermediate ear-trained keyboard player and are looking for chords to songs, you have come to the right spot.</p>
 
-            <p>In this series, wou will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
+            <p>In this series, you will have access to a ton of songs you've always wanted to learn but just never had the time.</p>
             <p><strong>What You\'ll Receive</strong></p>
             <ul class="no-indent">
                 <li>Sheet music written specifically for ear-trained learners</li>
